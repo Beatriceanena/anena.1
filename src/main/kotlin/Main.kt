31c393623364362a65e5895c0d5c2f46b20d6942
beatriceanena <anenabeattrice@gmail.com>
2022-02-12 09:54:34 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    var studentname="Anena Beatrice"
+    println(studentname)
+    var age =20
+    println(age)
+    var phonenumber: Long = 256758185675
+    println(phonenumber)
+    var weight: String= "40kg"
+    println(weight)
+    val iskenyan: Boolean= true
+    val isnotkenyan: Boolean= false
+    println(iskenyan) //output true if kenyan
+    println(isnotkenyan) //output false for non kenyan
+
+
+
+}
